4a2b2168c653522c472c6355ff1ca3a1526c60de
Linus <linus_koh@mymail.sutd.edu.sg>
2024-06-25 18:09:49 +0800

    Step Definitions Implementation in movie_steps.rb

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -5,19 +5,18 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
-Then /(.*) seed movies should exist/ do |n_seeds|
+Then /(\d+) seed movies should exist/ do |n_seeds|
   expect(Movie.count).to eq n_seeds.to_i
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
+# Make sure that one string (regexp) occurs before or after another one on the same page
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   page.body.should =~ /#{Regexp.escape(e1)}.*#{Regexp.escape(e2)}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+# "When I uncheck the following ratings: PG, G, R"
+# "When I check the following ratings: G"
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   rating_list.split(', ').each do |rating|
     if uncheck
@@ -34,14 +33,14 @@ Then /I should see all the movies/ do
 end
 
 # Additional steps to support filtering and checking movie visibility
-Then /I should see the following movies: (.*)/ do |movie_list|
-  movie_list.split(', ').each do |movie|
+Then /I should see the following movies:$/ do |movies_table|
+  movies_table.raw.flatten.each do |movie|
     expect(page).to have_content(movie)
   end
 end
 
-Then /I should not see the following movies: (.*)/ do |movie_list|
-  movie_list.split(', ').each do |movie|
+Then /I should not see the following movies:$/ do |movies_table|
+  movies_table.raw.flatten.each do |movie|
     expect(page).not_to have_content(movie)
   end
 end
